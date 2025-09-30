f4acd9794ca2c30a197e8605a601d35fab65907e
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2025-09-30 20:33:28 -0300

    feat: Busca por um ou demais
    Agora é possível realizar a busca dos dados de um ou mais animês para obter diferentes insights de acordo com o que deseja.

--- messages.js
+++ messages.js
@@ -1,6 +1,6 @@
-const data = {
+const messages = {
     ANILIST_IS_DOWN: "A API do Anilist está fora do ar!",
     RATE_LIMITING: "Você atingiu o limite de request da API do Anilist por minuto. Espere mais um minuto para prosseguir. O seu total de requests no último minuto foi: "
 }
 
-module.exports = { data }
+module.exports = { messages }
--- myanimelist.js
+++ myanimelist.js
@@ -1,90 +1,120 @@
 const jsdom = require("jsdom");
 const { JSDOM }= jsdom;
-const { data } = require("./messages.js");
+const { messages } = require("./messages.js");
 
-function dataProcessing (dados) {
-    animeName = dados.data.Media.title.english;
-    MALCode = dados.data.Media.idMal;
-    
-    animeName = animeName.split(' ').join('_');
+function graphQLStruct (filters_data, strictSearch) {
+    let [query_struct, media_struct ]= ['', ''];
+    let variables = {};
 
-    return [animeName, MALCode];
-}
+    const filters_keys = Object.keys(filters_data);
 
-function graphQLSearch (filter_test, filters_data) {
-    
-    let query_struct = '';
+    for(let i = 0; i < filters_keys.length; i++) {
+        if(filters_data[filters_keys[i]][0] !== false) {
+            query_struct += `$${filters_keys[i]}: ${filters_data[filters_keys[i]][1]}, `;
+            media_struct += `${filters_keys[i]}: $${filters_keys[i]}, `;
 
-    for(let i = 0; i < filter_test.length; i++) {
-        if
+            variables[filters_keys[i]] = filters_data[filters_keys[i]][0];
+        }
     }
 
-    let query = `
-    query ($search: String, $season: MediaSeason) {
-        Page (page: $page, perPage: $perPage) {
-            pageInfo {
-            currentPage
-            hasNextPage
-            perPage
-        }
-        Media (search: $search, type: ANIME) {
-            idMal
-            title {
-                english
+    let query;
+    if (strictSearch === false) {
+        variables['page'] = 1;
+        variables['perPage'] = 50;
+
+        query = 
+        `query (${query_struct}$page: Int, $perPage: Int) {
+            Page (page: $page, perPage: $perPage) {
+                pageInfo {
+                    currentPage
+                    hasNextPage
+                }
+                media (${media_struct}type: ANIME) {
+                    idMal
+                }
             }
-        }
+        }`;
+    } else {
+        query_struct = query_struct.slice(0, query_struct.length - 2);
+
+        query = 
+        `query (${query_struct}) {
+            Media (${media_struct}type: ANIME) {
+                idMal
+            }
+        }`;
     }
-    `;
+
+    return [query, variables];
 }
 
-async function extractMALcodeList (filter_test, filters_data) {
-    const nameList = [];
-    const MALCodeList = [];
-    
-    data = graphQLSearch(filter_test, filters_data);
-    // Query do GraphQL;
-
-
-    /*
+async function graphqlSearch (query, variables) {
     let data = await fetch('https://graphql.anilist.co', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json'
-            },
-            body: JSON.stringify({
-                query: query,
-                variables: variables
-            })}
-        );
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })}
+    );
 
-        switch(data.status) {
-            case 403:
-                console.log(data.ANILIST_IS_DOWN);
-                throw new Error(data.ANILIST_IS_DOWN);
-            case 429:
-                // Caso seja necessário conferir alguma vez, pode ser visto no header do request em 'x-ratelimit-limit' como pode ser visto na documentação ('https://docs.anilist.co/guide/rate-limiting')
-                const quantidade_request_anilist = 30;
-                throw new Error(data.RATE_LIMITING + quantidade_request_anilist);
-        }
-
-    for(let i = 0; i < animeList.length; i++) {
-        data = await dados.json();
-        console.log(dados)
-        let [animeName, MALCode] = dataProcessing(data);
-
-        nameList.push(animeName);
-        MALCodeList.push(MALCode);
+    switch(data.status) {
+        case 403:
+            console.log(messages.ANILIST_IS_DOWN);
+            throw new Error(messages.ANILIST_IS_DOWN);
+        case 429:
+            // Caso seja necessário conferir alguma vez, pode ser visto no header do request em 'x-ratelimit-limit' como pode ser visto na documentação ('https://docs.anilist.co/guide/rate-limiting')
+            const quantidade_request_anilist = 30;
+            throw new Error(messages.RATE_LIMITING + quantidade_request_anilist);
     }
 
-    return [nameList, MALCodeList]
-    */
+    data = await data.json();
+    data = data.data;
+
+    return data;
 }
 
-async function fetchMALpage (name, MALcode) {
-    myanimelist_site = await fetch(`https://myanimelist.net/anime/${MALcode}/${name}`).then((site) => { return site.text() } )
+async function requestMALcodeList (filters_data) {    
+    const strictSearch = filters_data['strictSearch'];
+    delete filters_data.strictSearch;
 
-    const dom = new JSDOM(myanimelist_page).window.document.querySelector("html").outerHTML;
+    const [query, variables] = graphQLStruct(filters_data, strictSearch);
+
+    let MALCodeList = [];
+
+    while(true) {
+        data = await graphqlSearch(query, variables);
+
+        if(data?.Page !== undefined) {
+            const perPage = data.Page.media.length;
+            for(let i = 0; i < perPage; i++) {
+                const idMal = data.Page.media[i]?.idMal;
+                if(idMal !== null) { MALCodeList.push(idMal); }
+            }
+            const hasNextPage = data.Page.pageInfo.hasNextPage;
+            if (hasNextPage) {
+                variables.page += 1;
+            } else {
+                break;
+            }
+        } else {
+            MALCodeList.push(data.Media.idMal);
+            break;
+        }
+    }
+    return MALCodeList;   
+}
+
+async function scrappingMALpage (MALcodeList) {
+    for(let i = 0; i < MALcodeList.length; i++) {
+        let myanimelist_site = await fetch(`https://myanimelist.net/anime/${MALcodeList[i]}`).then((site) => { return site.text() } );
+        
+        const dom = new JSDOM(myanimelist_site).window.document.querySelector("html").outerHTML;
+        console.log(dom)
+    }
 }
 
 async function main() {
@@ -96,35 +126,25 @@ async function main() {
         genre_in:             [false, '[String]'],
         popularity_greater:   [false, 'Int'],
         popularity_lesser:    [false, 'Int'],
-        search:               [false, 'String'],
+        search:               ["Spy x Family Season 3", 'String'],
         season:               [false, 'MediaSeason'],
         seasonYear:           [false, 'Int'],
         sort:                 [false, 'MediaSort'],
         source:               [false, 'MediaSource'],
         startDate_greater:    [false, 'FuzzyDateInt'],
-        startDate_lesser:     [false, 'FuzzyDateInt']
+        startDate_lesser:     [false, 'FuzzyDateInt'],
+        strictSearch:         true
     }
 
-    let animeNameList = [];
-    let MALCodeList = [];
-
+    let MALCodeList;
     try {
-        [animeNameList, MALCodeList] = await extractMALcodeList(filter_test, filters_data);
+        MALCodeList = await requestMALcodeList(filters_data);
     } catch (e) {
         console.error(e);
     }
 
-    console.log(animeNameList);
-    console.log(MALCodeList);
-
-    /*
-    for(let i = 0; i < nameList.length - 1; i++) {
-        let MALpage = await fetchMALpage(animeNameList[i], MALCodeList[i]);
-    }
-
-    return myanimelist_page;
-    */
-};
+    await scrappingMALpage(MALCodeList);
+}
 main();
 
 // extrair: score, usuários, ranked, popularidade, membros, temporada, gênero
